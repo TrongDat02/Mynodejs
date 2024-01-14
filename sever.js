a90aeb302291826e860609bc378a675871aa975a
unknown <aemkbg@gmail.com>
2024-01-14 18:28:37 +0700

    sang route moi o file moi roi

--- sever.js
+++ sever.js
@@ -1,11 +1,24 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+const app = express()// app express
+const port = 8080
+
+//config template engine
+app.set('views', './src/views/')
+app.set('view engine', 'ejs')
+
+//khai bao route
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+    res.send('Hello World! toi la dat')
+})
+
+app.get('/view', (req, res) => {
+    //res.send('Hello World! toi la dat')
+    res.render('vidu.ejs')
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
